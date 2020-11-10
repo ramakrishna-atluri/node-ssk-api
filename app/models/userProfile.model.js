7f6e567b3ac92cf82d6d6cc4c52efe15e7ed5743
deepakmelugiri <melugiri.deepak@gmail.com>
2020-11-09 21:26:10 -0500

    create profile

--- app/models/userProfile.model.js
+++ app/models/userProfile.model.js
@@ -2,24 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  profileId :{type: String, required: true},
-  basicinfo:{
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    dob: { type: String, required: true },
-    maritalStatus: { type: String, required: true },
-    gender: { type: String, required: true },
-    height: { type: String, required: true },
+  userProfileId :{type: String, required: false},
+  userId :{type: String, required: false},
+  basicInfo:{
+    firstName: { type: String, required: false },
+    lastName: { type: String, required: false },
+    dob: { type: String, required: false },
+    maritalStatus: { type: String, required: false },
+    gender: { type: String, required: false },
+    height: { type: String, required: false },
     bloodGroup: { type: String, required: false },
     bodyWeight: { type: String, required: false },
-    disability: { type: Boolean, required: true }
+    disability: { type: String, required: false }
   },
   familyInfo:{
-    fatherName: { type: String, required: true },
-    motherName: { type: String, required: true },
-    fatherStatus: { type: String, required: true },
-    motherStatus: { type: String, required: true },
-    nativePlace: { type: String, required: true },
+    fatherName: { type: String, required: false },
+    motherName: { type: String, required: false },
+    fatherStatus: { type: String, required: false },
+    motherStatus: { type: String, required: false },
+    nativePlace: { type: String, required: false },
     numberOfSiblings: { type: String, required: false },
     familyType: { type: String, required: false },
     familyValues: { type: String, required: false },
@@ -41,10 +42,8 @@ const schema = new Schema({
     city: {type: String, required: false}
   },
   contactInfo:{
-    email: {type: String, required: true},
-    countryCode: {type: String, require: true},
-    phone: {type: String, required: true},
-    emailVerified: {type: Boolean, required: false, default: false},
+    countryCode: {type: String, require: false},
+    contactNumber: {type: String, required: false},
     phoneVerified: {type: Boolean, required: false, default: false},
   }
 },
